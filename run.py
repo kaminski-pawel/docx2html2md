c253b04b9e8920a233a2d8dd0809adcdf4b7c829
Pawel Kaminski <pawel.kaminski@uni.lu>
2024-02-05 16:40:14 +0100

    Handle all transformations inside data/ dir

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import datetime
+import hashlib
 import shutil
 import subprocess
 import time
 import bs4
+import os
 
 # from bs4 import BeautifulSoup
 import mammoth
@@ -10,6 +13,8 @@ import pathlib
 # from markdownify import markdownify as md
 from html2md import convert_to_md
 from citations import add_citations_and_bibliography
+
+DATA_DIR = "data/"
 
 # https://jupyterbook.org/en/stable/reference/cheatsheet.html#executable-code
 # ENABLE_EXECUTABLE_CODE_IN_JB = """---
@@ -24,6 +29,12 @@ from citations import add_citations_and_bibliography
 # ---
 
 # """
+
+
+def _copy_to_data_dir(fp: pathlib.Path) -> pathlib.Path:
+    newfp = pathlib.Path(DATA_DIR, fp.name)
+    shutil.copyfile(fp, newfp)
+    return newfp
 
 
 def _prepare_md(fp: pathlib.Path) -> None:
@@ -54,32 +65,52 @@ def _prepare_md(fp: pathlib.Path) -> None:
 
 def _render_digital_version(fp: pathlib.Path) -> None:
     # prepare a jupyter-notebook and mystmd rendering of the .md file
-    shutil.copyfile(fp.with_suffix(".md"), f"jb/{fp.with_suffix('.md').name}")
+    # shutil.copyfile(fp.with_suffix(".md"), f"jb/{fp.with_suffix('.md').name}")
     shutil.copyfile(fp.with_suffix(".md"), f"mystmd/{fp.with_suffix('.md').name}")
-    subprocess.run(
-        "jupyter-book build jb/".split(" ")
-    )  # when tested around 1.5 sec perf_time() 😬
-    subprocess.Popen("myst build --html".split(" "), cwd="mystmd/")
+    # subprocess.run(
+    #     "jupyter-book build jb/".split(" ")
+    # )  # when tested around 1.5 sec perf_time() 😬
     WORKS_GREAT_BUT_IS_SLOWER_THAN_JB = (
         'subprocess.Popen("myst build --html".split(" "), cwd="mystmd/")'
         # hard to measure: builds fast, but then has to spin up a server
     )
 
 
+BOOK_PATH = "/mnt/c/Users/Pawel.kaminski/OneDrive - University of Luxembourg/Digital Monography Documents/Guidelines for Drafting Manuscripts"
+LATENCY_TEST = "/mnt/c/Users/Pawel.kaminski/OneDrive - University of Luxembourg/Digital Monography Documents/2024-02-05_review-january/latency-test.docx"
+
+
 if __name__ == "__main__":
+    last_modified = {}
     while True:
         for fp in [
             # pathlib.Path("./citation_in_fields.docx"),
             # pathlib.Path("./citation_in_footnotes.docx"),
             # pathlib.Path("./cross-references.docx"),
-            pathlib.Path("./docx_guidelines.docx"),
-            # pathlib.Path("./quick-loop.docx")
+            # pathlib.Path("./docx_guidelines.docx"),
+            # pathlib.Path("./quick-loop.docx"),
+            pathlib.Path("./local-latency-test.docx"),
+            pathlib.Path(LATENCY_TEST),
         ]:
-            assert fp.exists()
-            assert fp.is_dir() == False
-            print(fp)
-            _prepare_md(fp)
-            _render_digital_version(fp)
-        print("\n> sleep")
-        time.sleep(10)
-        break
+            # pass
+            # for fp in pathlib.Path(BOOK_PATH).glob("*.docx"):
+            if not fp.exists() or fp.is_dir():
+                continue
+            if fp.name.startswith("~$"):
+                continue
+            if fp not in last_modified:
+                last_modified[fp] = fp.stat().st_mtime
+            # print(
+            #     "mtime=",
+            #     datetime.datetime.fromtimestamp(fp.stat().st_mtime),
+            #     "; cached=",
+            #     datetime.datetime.fromtimestamp(last_modified[fp]),
+            # )
+            if fp.stat().st_mtime > last_modified[fp]:
+                "Update recognized"
+                localfp = _copy_to_data_dir(fp)
+                _prepare_md(localfp)
+                _render_digital_version(localfp)
+                last_modified[fp] = fp.stat().st_mtime
+            time.sleep(2)
+            print("...")
